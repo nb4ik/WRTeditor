d87c2004b24936a2234be4e2754ea06c17386069
nb4ik <danilbondin123@gmail.com>
2026-09-12 19:20:58 +0600

    interface updateТестовый дизайн

---
+++ WRT/ConnectSSH.cs
@@ -0,0 +1,18 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WRT
+{
+    public static class ConnectSSH
+    {
+        public static void Connect( string username, string password, string host = "192.168.1.1")
+        {
+            
+        }
+
+    }
+}
--- WRT/MainWindow.xaml.cs
+++ WRT/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Renci.SshNet;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,46 @@ namespace WRT
     /// </summary>
     public partial class MainWindow : Window
     {
+        ShellStream globalStreamRead;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ConnectToServer(object sender, RoutedEventArgs e)
+        {
+            string username = loginBox.Text;
+            string password = passwordBox.Text;
+
+            using (var client = new SshClient("192.168.1.1", username, password))
+            {
+                try
+                {
+                    client.Connect();
+                    globalStreamRead = client.CreateShellStream("custom_term", 80, 24, 800, 600, 1024);
+                    globalStreamRead.WriteLine("sh <(wget -O - https://raw.githubusercontent.com/StressOzz/Zapret-Manager/main/Zapret-Manager.sh)");
+                    System.Threading.Thread.Sleep(3000);
+
+                    string output = globalStreamRead.Read();
+                    string[] lines = output.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    ZapVer.Text = lines[43];
+                    Proxy.Text = lines[44];
+                    Hosts.Text = lines[45];
+                    Strategy.Text = lines[46];
+                }
+                catch (Exception ex) { }
+
+            }
+        }
+
+        private void MenuButtonPress(object sender, RoutedEventArgs e, string output)
+        {
+            try
+            {
+                globalStreamRead.WriteLine(output);
+            }
+            catch (Exception ex) { }
         }
     }
 }
