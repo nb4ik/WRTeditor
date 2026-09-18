7cf059c4171b5aa89000d3cb5945342479c3c971
nb4ik <danilbondin123@gmail.com>
2026-09-18 15:00:15 +0600

    added connection status

---
+++ WRT/ShellStreamExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Renci.SshNet;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WRT
+{
+    public static class ShellStreamExtensions
+    {
+        public static Task<string> ExpectAsync(this ShellStream stream, Regex pattern, TimeSpan timeout)
+        {
+            var tcs = new TaskCompletionSource<string>();
+            var expectAction = new ExpectAction(pattern, text => tcs.TrySetResult(text));
+
+            try
+            {
+                stream.BeginExpect(new[] { expectAction });
+            }
+            catch (Exception ex)
+            {
+                tcs.TrySetException(ex);
+            }
+
+            var cancellationTokenSource = new CancellationTokenSource(timeout);
+            cancellationTokenSource.Token.Register(() => tcs.TrySetCanceled());
+
+            return tcs.Task;
+        }
+    }
+}
